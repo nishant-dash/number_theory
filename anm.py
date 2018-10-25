066c2d680ef18344a546df7081c743046b54592f
Nishant Dash <dash3@illinois.edu>
2018-10-25 17:36:45 -0500

    messy, working on some stuff

--- anm.py
+++ anm.py
@@ -11,6 +11,7 @@ class primeFactorsObject():
         self.largest_factor = 0
         self.smallest_factor = math.inf
         self.num_factors = 0
+        self.unique_factors = set()
         self.prime_factors = {}
         self.number = 1
 
@@ -30,6 +31,8 @@ class primeFactorsObject():
 
         self.number *= x
 
+        self.unique_factors.add(x)
+
     def remove_factor(self, x):
         if x in self.prime_factors:
             if self.prime_factors[x] == 1:
@@ -38,6 +41,9 @@ class primeFactorsObject():
                 self.prime_factors[x] -= 1
         else:
             return
+
+        if x not in self.prime_factors:
+            self.unique_factors.remove(x)
 
         self.num_factors -= 1
         if self.num_factors == 0:
@@ -109,8 +115,8 @@ def primeFactorization_condensed(n):
 
 @lru_cache(maxsize=256)
 def Moebius(n):
-    if n == 1:
-        return 1
+    if n <= 1:
+        return n
 
     pF = primeFactorization(n)
 
@@ -120,9 +126,9 @@ def Moebius(n):
     return 0
 
 
-def char_fn_square(n):
+def something(n):
     summation = 0
-    rl = [i for i in range(int(math.sqrt(n)))]
+    rl = [i + 1 for i in range(int(math.sqrt(n)))]
 
     for i in rl:
         d = i + 1
@@ -132,9 +138,30 @@ def char_fn_square(n):
     return summation
 
 
+# returns true if a number is squarefull
+def is_a_squarefull_number(n):
+    pF = primeFactorization_condensed(n)
+    for k, v in pF:
+        if v < 2:
+            return False
+
+    return True
+
+# returns true if a number is squarefree
+
+
+def is_a_squarefree_number(n):
+    pF = primeFactorization_condensed(n)
+    for k, v in pF:
+        if v >= 2:
+            return False
+
+    return True
+
+
 def testing_fn():
     for i in range(1000):
-        x = char_fn_square(i)
+        x = something(i)
         if (x != 0):
             pf = primeFactorization(i)
             print("f(" + str(i) + ") = ", x, "with prime factors ",
@@ -142,7 +169,7 @@ def testing_fn():
 
     sqs = [i for i in [4, 16, 25, 75, 48]]
     for i in sqs:
-        print(char_fn_square(i))
+        print(something(i))
 
 
 def EulerPhi(n):
@@ -160,7 +187,7 @@ def visualize():
     factor = 9
 
     for num in range(0, factor * 100):
-        temp = char_fn_square(num)
+        temp = something(num)
         fs.append(temp)
         # print(strs)
 
@@ -182,13 +209,9 @@ def visualize():
 
 
 def desmos():
-    data = [Moebius(i + 1) for i in range(100)]
-    with open('to_plot.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(data)
-    csvfile.close()
-    data = [EulerPhi(i + 1) for i in range(100)]
-    with open('to_plot2.csv', 'w') as csvfile:
+    # data = [Moebius(i) for i in range(1, 1000)]
+    data = [-1 * Moebius(int((2**(i - 1))**2)) for i in range(0, 10)]
+    with open('to_plot12.txt', 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(data)
     csvfile.close()
@@ -214,4 +237,107 @@ def desmos():
 #     invEP_list.add(invEP)
 #     print("InvEulerPhi(" + str(x) + ") = ", invEP)
 
-desmos()
+
+def char_fn_squarefree(n):
+    if is_a_squarefree_number(n):
+        return 1
+    return 0
+
+
+def num_sqfull_lessthanx(x):
+    sqfull = 0
+    num = x
+    for i in range(num):
+        if is_a_squarefull_number(i + 1):
+            sqfull += 1
+    # print("Number of squarefull numbers less than ", num, " is ", sqfull)
+    return sqfull
+
+
+def b(num):
+    s = 0
+    for b in range(1, int(num**(1 / 3))):
+        if is_a_squarefree_number(b):
+            temp = char_fn_squarefree(b)
+            # if temp == 1:
+            #     print(b, end=' \n')
+            s += temp
+    # print("Summation of u^2(squarefree numbers less than ",
+          # int(num**(1 / 3)) + 1, ") is ", s)
+    return s
+
+
+# desmos()
+
+
+# def main_ans(x, a):
+#     # odd numbers less than x/a
+#     count = 0
+#     for i in range(0, (x // a) + 1):
+#         even = i % 2 == 0
+#         if not even:
+#             # print(i, end=' ')
+#             count += 1
+#     # print(" count is ", count)
+#     return count
+
+
+# def check(x, a):
+#     # print(' and ', (x + a) // (2 * a))
+#     # return (x + a) // (2 * a)
+#     return (x // a - 1) // 2 + 1
+
+
+# val = True
+# for a in range(3, 100):
+#     for x in range(1, 125):
+#         if (x < a):
+#             continue
+#         else:
+#             val &= main_ans(x, a) == check(x, a)
+# print(val)
+
+def omega(n):
+    return len(primeFactorization_condensed(n).unique_factors)
+
+
+def special_omega(n):
+    return 2 ** omega(n)
+
+
+def d(n):
+    count = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            count += 1
+    return count
+
+
+def d_alt(n):
+    p = 1
+    x = primeFactorization_condensed(n).prime_factors
+    for k, v in x.items():
+        p *= (v + 1)
+    return p
+# main_ans(10, 3)
+
+
+def sum_of_div(n):
+    p = 1
+    x = primeFactorization_condensed(n).prime_factors
+    for k, v in x.items():
+        p *= ((-1 / (k**(v))) + k) / (k - 1)
+    return p
+
+
+def perfect_number(p):
+    # if p not prime, throw and error
+    return (2**(p - 1)) * ((2**p) - 1)
+
+
+# desmos()
+num = [2, 3, 5, 7, 11, 13, 17, 19]
+for i in num:
+    print(perfect_number(i))
+# print(d_alt())
+print(sum_of_div(num))
